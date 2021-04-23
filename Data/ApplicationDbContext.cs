72df6d1ab249ede8a1c0a642e02ffe61b49c990f
Kendyrkhan <kendyrkhan.i@gmail.com>
2021-04-23 16:45:50 +0600

    milestone1

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Kinogo.Models;
+
+
+namespace Kinogo.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        public DbSet<Comment> Comments { get; set; }
+
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kinogo.Models
+{
+    public class Movie
+    {
+
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Year { get; set; }
+        public string Image { get; set; }
+        public string Сollection { get; set; }
+        public string Spent { get; set; }
+        public double Rating { get; set; }
+
+        public long CountryId { get; set; }
+        public Country Country { get; set; }
+        public List<Comment> Comments { get; set; }
+
+    }
+}
